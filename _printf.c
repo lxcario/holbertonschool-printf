7176dca94b386e351db2afc3576ecee92f333621
lxcario <resquedzn05@gmail.com>
2025-07-20 18:38:07 +0000

    yes

--- _printf.c
+++ _printf.c
@@ -2,9 +2,10 @@
 
 static int handle_specifier(const char *format, int *i,
 			    va_list args, spec_t funcs[]);
+static int print_unsigned_number(unsigned int n);
 
 /**
- * _printf - A custom implementation printf function.
+ * _printf - A custom implementation of the printf function.
  * @format: The format string.
  *
  * Return: The number of characters printed.
@@ -18,6 +19,8 @@ int _printf(const char *format, ...)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
 		{NULL, NULL}
 	};
 
@@ -129,3 +132,225 @@ int print_percent(__attribute__((unused))va_list args)
 	return (1);
 }
 
+/**
+ * print_integer - Prints an integer for %d and %i specifiers.
+ * @args: The va_list containing the integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_integer(va_list args)
+{
+	int n = va_arg(args, int);
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	count += print_unsigned_number(num);
+	return (count);
+}
+
+/**
+ * print_unsigned_number - Prints an unsigned integer recursively.
+ * @n: The unsigned integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+static int print_unsigned_number(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10)
+	{
+		count += print_unsigned_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+	count++;
+	return (count);
+}
+#include "main.h"
+
+static int handle_specifier(const char *format, int *i,
+			    va_list args, spec_t funcs[]);
+static int print_unsigned_number(unsigned int n);
+
+/**
+ * _printf - A custom implementation of the printf function.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+	int printed_chars;
+	va_list args;
+	spec_t funcs[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{NULL, NULL}
+	};
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			printed_chars = handle_specifier(format, &i, args, funcs);
+			if (printed_chars == -1)
+			{
+				va_end(args);
+				return (-1);
+			}
+			count += printed_chars;
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
+
+/**
+ * handle_specifier - Handles the format specifier logic for _printf.
+ * @format: The format string.
+ * @i: Pointer to the current index in the format string.
+ * @args: The va_list of arguments.
+ * @funcs: The array of specifier-function mappings.
+ *
+ * Return: The number of characters printed by the handler.
+ */
+static int handle_specifier(const char *format, int *i,
+			    va_list args, spec_t funcs[])
+{
+	int j = 0, count = 0;
+
+	(*i)++;
+
+	if (format[*i] == '\0')
+		return (-1);
+
+	while (funcs[j].spec != NULL)
+	{
+		if (*(funcs[j].spec) == format[*i])
+		{
+			count += funcs[j].f(args);
+			return (count);
+		}
+		j++;
+	}
+
+	_putchar('%');
+	_putchar(format[*i]);
+	return (2);
+}
+
+/**
+ * print_char - Prints a character.
+ * @args: A list of arguments pointing to the character to be printed.
+ *
+ * Return: 1 (for one character printed).
+ */
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: A list of arguments pointing to the string to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	int i;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (i);
+}
+
+/**
+ * print_percent - Prints a percent symbol.
+ * @args: A list of arguments (unused).
+ *
+ * Return: 1 (for one character printed).
+ */
+int print_percent(__attribute__((unused))va_list args)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * print_integer - Prints an integer for %d and %i specifiers.
+ * @args: The va_list containing the integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_integer(va_list args)
+{
+	int n = va_arg(args, int);
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	count += print_unsigned_number(num);
+	return (count);
+}
+
+/**
+ * print_unsigned_number - Prints an unsigned integer recursively.
+ * @n: The unsigned integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+static int print_unsigned_number(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10)
+	{
+		count += print_unsigned_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+	count++;
+	return (count);
+}
