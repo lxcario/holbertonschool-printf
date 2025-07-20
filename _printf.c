d32fbdbb620ee98f4e15f5a6f35419d1092bb861
lxcario <resquedzn05@gmail.com>
2025-07-20 18:19:21 +0000

    yes

--- _printf.c
+++ _printf.c
@@ -1,5 +1,81 @@
 #include "main.h"
 
+static int handle_specifier(const char *format, int *i,
+			    va_list args, spec_t funcs[]);
+
+/**
+ * _printf - A custom implementation of the printf function.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+	va_list args;
+	spec_t funcs[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			count += handle_specifier(format, &i, args, funcs);
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
+
+/**
+ * handle_specifier - Handles the format specifier logic for _printf.
+ * @format: The format string.
+ * @i: Pointer to the current index in the format string.
+ * @args: The va_list of arguments.
+ * @funcs: The array of specifier-function mappings.
+ *
+ * Return: The number of characters printed by the handler.
+ */
+static int handle_specifier(const char *format, int *i,
+			    va_list args, spec_t funcs[])
+{
+	int j = 0, count = 0;
+
+	(*i)++;
+
+	if (format[*i] == '\0')
+		return (-1);
+
+	while (funcs[j].spec != NULL)
+	{
+		if (*(funcs[j].spec) == format[*i])
+		{
+			count += funcs[j].f(args);
+			return (count);
+		}
+		j++;
+	}
+
+	_putchar('%');
+	_putchar(format[*i]);
+	return (2);
+}
+
 /**
  * print_char - Prints a character.
  * @args: A list of arguments pointing to the character to be printed.
@@ -46,60 +122,3 @@ int print_percent(__attribute__((unused))va_list args)
 	return (1);
 }
 
-/**
- * _printf - A custom implementation of the printf function.
- * @format: The format string.
- *
- * Return: The number of characters printed.
- */
-int _printf(const char *format, ...)
-{
-	int i = 0, count = 0, j;
-	va_list args;
-	spec_t funcs[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{NULL, NULL}
-	};
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(args, format);
-
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
-
-			j = 0;
-			while (funcs[j].spec != NULL)
-			{
-				if (*(funcs[j].spec) == format[i])
-				{
-					count += funcs[j].f(args);
-					break;
-				}
-				j++;
-			}
-			if (funcs[j].spec == NULL)
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		i++;
-	}
-	va_end(args);
-	return (count);
-}
